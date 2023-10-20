fa9bd767e4091bade9521ab22d594bbaac8bee80
Sultanov Ruslan <sultrus15@gmail.com>
2023-10-20 23:06:01 +0300

    First task

---
+++ 01_CLI_Interactive_Sort/main.mjs
@@ -0,0 +1,67 @@
+import { greeting, condition, farewell } from './text.js';
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let array;
+
+start();
+
+function start() {
+    rl.question(greeting, words => {
+        if (words.length < 10) {
+            console.log('Please, input 10 words.');
+            start();
+            return
+        }
+        array = words.split(' ');
+
+        rl.question(condition, answear => {
+            if(answear === 'exit'){
+                console.log(farewell);
+                return rl.close();
+            }
+            switch (Number(answear)) {
+                case 1:
+                    console.log(separateWords('w', array).sort());
+                    break;
+                case 2:
+                    console.log(separateWords('n', array).sort());
+                    break;
+                case 3:
+                    console.log(separateWords('n', array).sort((a,b)=>b-a));
+                    break;
+                case 4:
+                    console.log(separateWords('w', array).sort((a,b)=>a.length-b.length));
+                    break;
+                case 5:
+                    const setWords = new Set(separateWords('w', array));
+                    console.log(Array.from(setWords));
+                    break;
+                case 6:
+                    const setAll = new Set(array);
+                    console.log(Array.from(setAll));
+                    break;
+                default:
+                    console.log(array);
+            }
+            start();
+            return
+        })
+    })
+
+
+}
+
+
+function separateWords(type, array) {
+    switch (type) {
+        case 'n':
+            return array.filter((value) => value.match(/^[0-9]*$/))
+        case 'w':
+            return array.filter((value) => value.match(/[A-Za-z]/))
+    }
+}
---
+++ 01_CLI_Interactive_Sort/text.js
@@ -0,0 +1,23 @@
+const greeting = 
+"Hello. Enter 10 words or digits deviding them in spaces: ";
+const condition = 
+`\nHow woud you like to sort your array:
+1. Sort words alphaberically;
+2. Show numbers from lesser to greater;
+3. Show numbers from bigger to smaller;
+4. Display words in ascending oder by numbers;
+5. Show only unique words;
+6. Display only unique values from the of words and numbers entered by the user;
+To exit the program enter word "exit"
+
+Select (1-7) and press ENTER:`;
+const farewell = 
+"\nGood bye! Come back again!"
+
+module.exports = {
+    greeting,
+    condition,
+    farewell
+}
+
+
